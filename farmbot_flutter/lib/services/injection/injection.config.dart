aa61b79d06a1717c6a9607fdf8f3be723314248c
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-06-30 18:36:41 -0500

    inj update

--- farmbot_flutter/lib/services/injection/injection.config.dart
+++ farmbot_flutter/lib/services/injection/injection.config.dart
@@ -7,10 +7,10 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../app/home/application/home/home_cubit.dart' as _i5;
+import '../../app/home/application/home/home_cubit.dart' as _i6;
 import '../../app/home/domain/repo/home/home_repo.i.dart' as _i3;
-import '../../app/home/infrastructure/home/home_entity.dao.dart'
-    as _i4; // ignore_for_file: unnecessary_lambdas
+import '../../app/home/infrastructure/home/home_entity.dao.dart' as _i4;
+import '../logging/logger.dart' as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -20,11 +20,12 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.factory<_i3.IHomeCache>(() => _i4.HomeCache(), instanceName: 'HomeCache');
   gh.factory<_i3.IHomeSource>(() => _i4.HomeSource(),
       instanceName: 'HomeSource');
+  gh.factory<_i5.Logger>(() => _i5.Logger());
   gh.factory<_i3.IHomeRepo>(
       () => _i4.HomeEntity(get<_i3.IHomeCache>(instanceName: 'HomeCache'),
           get<_i3.IHomeSource>(instanceName: 'HomeSource')),
       instanceName: 'HomeEntity');
-  gh.factory<_i5.HomeCubit>(
-      () => _i5.HomeCubit(get<_i3.IHomeRepo>(instanceName: 'HomeEntity')));
+  gh.factory<_i6.HomeCubit>(
+      () => _i6.HomeCubit(get<_i3.IHomeRepo>(instanceName: 'HomeEntity')));
   return get;
 }
