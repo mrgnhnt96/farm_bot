69922066d64e3d38662f5c47506c00c333b68278
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-06-30 18:10:28 -0500

    removed counter from test

--- farmbot_flutter/test/app_test.dart
+++ farmbot_flutter/test/app_test.dart
@@ -6,14 +6,7 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:farmbot_flutter/app/app.dart';
-import 'package:farmbot_flutter/counter/counter.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
+  group('App', () {});
 }
