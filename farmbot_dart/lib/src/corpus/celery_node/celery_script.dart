0d1f966134d252818d7ded2b369ed4bac21365d1
Jonah Walker <supposedlysam@gmail.com>
2021-07-30 09:58:24 -0600

    refactor: remove isolated files

--- farmbot_dart/lib/src/corpus/celery_node/celery_script.dart
+++ farmbot_dart/lib/src/corpus/celery_node/celery_script.dart
@@ -1,7 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:farmbot/src/corpus/celery_node/script/celery_node.dart';
-import 'package:farmbot/src/corpus/enums.dart';
 
 part 'celery_script.freezed.dart';
 part 'celery_script.g.dart';
@@ -9,44 +8,6 @@ part 'celery_script.g.dart';
 abstract class CeleryScript {
   String get kind;
   CeleryNode toRequest();
-}
-
-@freezed
-class Assertion with _$Assertion implements CeleryScript {
-  const Assertion._();
-
-  const factory Assertion({
-    String? comment,
-    required AssertionArgs args,
-  }) = _DefaultAssertion;
-
-  factory Assertion.fromJson(Map<String, dynamic> json) =>
-      _$AssertionFromJson(json);
-
-  @override
-  String get kind => 'assertion';
-
-  @override
-  CeleryNode toRequest() {
-    return CeleryNode(
-      args: args.toJson(),
-      kind: kind,
-      comment: comment,
-      body: [],
-    );
-  }
-}
-
-@freezed
-class AssertionArgs with _$AssertionArgs {
-  const factory AssertionArgs({
-    @JsonKey(name: '_then') Execute? then,
-    required AllowedAssertionTypes assertionType,
-    required String lua,
-  }) = _DefaultAssertionArgs;
-
-  factory AssertionArgs.fromJson(Map<String, dynamic> json) =>
-      _$AssertionArgsFromJson(json);
 }
 
 @freezed
