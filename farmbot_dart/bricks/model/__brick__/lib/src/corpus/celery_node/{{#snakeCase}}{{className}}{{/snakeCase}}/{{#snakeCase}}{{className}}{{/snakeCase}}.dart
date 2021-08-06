db6fc50ba957eaff831a2eeb42c73d17c4a0e619
Morgan <mrgnhnt96@gmail.com>
2021-08-06 09:48:58 -0500

    refactor(dev): updated model to reflect the axis addition

--- farmbot_dart/bricks/model/__brick__/lib/src/corpus/celery_node/{{#snakeCase}}{{className}}{{/snakeCase}}/{{#snakeCase}}{{className}}{{/snakeCase}}.dart
+++ farmbot_dart/bricks/model/__brick__/lib/src/corpus/celery_node/{{#snakeCase}}{{className}}{{/snakeCase}}/{{#snakeCase}}{{className}}{{/snakeCase}}.dart
@@ -8,8 +8,31 @@ part '{{#snakeCase}}{{className}}{{/snakeCase}}.freezed.dart';
 part '{{#snakeCase}}{{className}}{{/snakeCase}}.g.dart';
 
 @freezed
-class {{#pascalCase}}{{className}}{{/pascalCase}} with _${{#pascalCase}}{{className}}{{/pascalCase}} {
-  const factory {{#pascalCase}}{{className}}{{/pascalCase}}() = _{{#pascalCase}}{{className}}{{/pascalCase}};
+class {{#pascalCase}}{{className}}{{/pascalCase}} with _${{#pascalCase}}{{className}}{{/pascalCase}} implements CeleryScript {
+  const {{#pascalCase}}{{className}}{{/pascalCase}}._();
+  const factory {{#pascalCase}}{{className}}{{/pascalCase}}({
+    String? comment,
+    required {{#pascalCase}}{{className}}{{/pascalCase}}Args args,
+  }) = _Default{{#pascalCase}}{{className}}{{/pascalCase}};
+
   factory {{#pascalCase}}{{className}}{{/pascalCase}}.fromJson(Map<String, dynamic> json) =>
       _${{#pascalCase}}{{className}}{{/pascalCase}}FromJson(json);
+
+  @override
+  String get kind => '{{#snakeCase}}{{className}}{{/snakeCase}}';
+
+  @override
+  CeleryNode toRequest() {
+    return CeleryNode(kind: kind, args: args.toJson(), body: []);
+  }
+}
+
+@freezed
+class {{#pascalCase}}{{className}}{{/pascalCase}}Args with _${{#pascalCase}}{{className}}{{/pascalCase}}Args {
+  const factory {{#pascalCase}}{{className}}{{/pascalCase}}Args({
+    required AllowedAxis axis,
+  }) = _{{#pascalCase}}{{className}}{{/pascalCase}}Args;
+
+  factory {{#pascalCase}}{{className}}{{/pascalCase}}Args.fromJson(Map<String, dynamic> json) =>
+      _${{#pascalCase}}{{className}}{{/pascalCase}}ArgsFromJson(json);
 }
